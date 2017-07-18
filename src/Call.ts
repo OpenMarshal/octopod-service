647c3aaedde2e5170b5605dabcb8c3543c3246de
Adrien Castex <adrien.castex@protonmail.com>
2017-07-18 16:10:05 +0200

    Added a 'cleanup' function to the callback of the 'call' function

--- src/Call.ts
+++ src/Call.ts
@@ -1,10 +1,14 @@
 import { Service } from './Service'
 
-export function call(coreUrl : string, service : string, method : string, inputData : any, callback : (response : any, responsePaths : { [method : string] : string[] }) => void) : void
-export function call<T>(coreUrl : string, service : string, method : string, inputData : any, callback : (response : T, responsePaths : { [method : string] : string[] }) => void) : void
-export function call<T, Q>(coreUrl : string, service : string, method : string, inputData : Q, callback : (response : T, responsePaths : { [method : string] : string[] }) => void) : void
-export function call<T, Q>(coreUrl : string, service : string, method : string, inputData : Q, callback : (response : T, responsePaths : { [method : string] : string[] }) => void) : void
+export function call(coreUrl : string, service : string, method : string, inputData : any, callback : (response : any, responsePaths : { [method : string] : string[] }, cleanup : () => void) => void) : void
+export function call<T>(coreUrl : string, service : string, method : string, inputData : any, callback : (response : T, responsePaths : { [method : string] : string[] }, cleanup : () => void) => void) : void
+export function call<T, Q>(coreUrl : string, service : string, method : string, inputData : Q, callback : (response : T, responsePaths : { [method : string] : string[] }, cleanup : () => void) => void) : void
+export function call<T, Q>(coreUrl : string, service : string, method : string, inputData : Q, callback : (response : T, responsePaths : { [method : string] : string[] }, cleanup : () => void) => void) : void
 {
     const s = new Service('', coreUrl);
-    s.call<T, Q>(service, method, inputData, callback);
+    s.call<T, Q>(service, method, inputData, (res, paths) => {
+        callback(res, paths, () => {
+            s.dispose(paths);
+        })
+    });
 }
