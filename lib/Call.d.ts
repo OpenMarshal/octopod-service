5f9bd7d8a394049ee0d2d01f29143115bf0fd66d
Adrien Castex <adrien.castex@protonmail.com>
2017-07-18 16:27:32 +0200

    Exported the 'ServiceReferenceInput' interface

--- lib/Call.d.ts
+++ lib/Call.d.ts
@@ -1,9 +1,9 @@
 export declare function call(coreUrl: string, service: string, method: string, inputData: any, callback: (response: any, responsePaths: {
     [method: string]: string[];
-}) => void): void;
+}, cleanup: () => void) => void): void;
 export declare function call<T>(coreUrl: string, service: string, method: string, inputData: any, callback: (response: T, responsePaths: {
     [method: string]: string[];
-}) => void): void;
+}, cleanup: () => void) => void): void;
 export declare function call<T, Q>(coreUrl: string, service: string, method: string, inputData: Q, callback: (response: T, responsePaths: {
     [method: string]: string[];
-}) => void): void;
+}, cleanup: () => void) => void): void;
--- src/ServiceReference.ts
+++ src/ServiceReference.ts
@@ -1,21 +1,25 @@
 
+export interface ServiceReferenceInput
+{
+    isVolatile ?: boolean
+    flushed ?: boolean
+    encrypt ?: boolean
+    mainOutputMethod ?: string
+    outputs ?: {
+        [method : string] : number
+    }
+}
+
 export interface ServiceReference
 {
     aliases ?: string[]
     inputs : {
-        [method : string] : {
-            isVolatile ?: boolean
-            flushed ?: boolean
-            encrypt ?: boolean
-            mainOutputMethod ?: string
-            outputs ?: {
-                [method : string] : number
-            }
-        }
+        [method : string] : ServiceReferenceInput
     }
 }
 
 export interface ServiceReferenceExtended extends ServiceReference
 {
     name : string
+    owner ?: string
 }
